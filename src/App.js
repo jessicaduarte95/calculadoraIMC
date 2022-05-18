4fd4f25f1e8147e2195c5a16e8911eff93000ba1
jessicaduarte95 <jessicaduarte.95@gmail.com>
2022-05-18 19:57:30 -0300

    Calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
+const tabelaIMC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <table border='1' style={{borderCollapse:'collapse'}}>
+      <thead>
+        <tr>
+          <th>
+            Classificação
+          </th>
+          <th>
+            IMC
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Abaixo do Peso</td>
+          <td>Abaixo de 18,5</td>
+        </tr>
+        <tr>
+          <td>Peso Normal</td>
+          <td>Entre 18,5 e 24,5</td>
+        </tr>
+        <tr>
+          <td>Sobrepeso</td>
+          <td>Entre 25 e 29,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade grau I</td>
+          <td>Entre 30 e 34,9</td>
+        </tr>
+        <tr>
+          <td>Obesidade grau II</td>
+          <td>Entre 35 e 39,9</td>
+        </tr>
+      </tbody>
+    </table>
+  )
 }
 
-export default App;
+const fpeso = (p,sp) => {
+  return(
+    <div>
+      <label>
+        Peso
+        <input type="text" value={p} onChange={(e)=>{sp(e.target.value)}}></input>
+      </label>
+    </div>
+  )
+}
+
+const faltura = (a,sa) => {
+  return(
+    <div>
+      <label>
+        Altura
+        <input type="text" value={a} onChange={(e)=>{sa(e.target.value)}}></input>
+      </label>
+    </div>
+  )
+}
+
+const fcalcular = (p,a,sr) => {
+  const calc = () => {
+    sr(p/(a*a))
+  }
+  return(
+    <div>
+      <button onClick={calc}>Calcular</button>
+    </div>
+  )
+}
+
+const fresultado = (r) => {
+  return(
+    <div>
+      <p>Resultado: {r.toFixed(2)}</p>
+    </div>
+  )
+}
+
+export default function App() {
+
+  const [peso,setPeso] = useState(0)
+  const [altura,setAltura] = useState(0)
+  const [resultado, setResultado] = useState(0)
+
+  return (
+    <div>
+      {fpeso(peso, setPeso)}
+      {faltura(altura, setAltura)}
+      {fcalcular(peso, altura, setResultado)}
+      {fresultado(resultado)}
+      {tabelaIMC()}
+    </div>
+  )
+}
